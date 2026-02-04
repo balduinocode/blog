bd7fc504167a35d58164075407b9c3da46aa7540
balduinocode <contato@alexmartins.work>
2026-02-04 19:17:07 -0300

    Nova nota

--- content/notes.tsx
+++ content/notes.tsx
@@ -8,27 +8,6 @@ export interface Note {
 
 export const notes: Note[] = [
   {
-    "slug": "amador",
-    "title": "O dom do amador",
-    "date": "Janeiro 2026",
-    "excerpt": "",
-    "content": "<p>Quando Jepp Gambardella, no filme \"La Grande Bellezza\", diz: \"É triste ser boa, corre o risco de tornar-se ágil\", eu fiquei um tanto curioso. O que ele quis dizer com isso? Busquei em algumas entrevistas do diretor Paolo Sorrentino a resposta. Para ele, o cinema, mas também a própria vida, é o reino dos dilettantes (amadores). Apenas os amadores conseguem ter liberdade criativa, pois sempre encontram-se na nascente — onde as ideias vêm infinitas pelo seio da terra, e não no oceano — onde tudo se dilui. Além disso, todo artista em essência é um amador, pois na etimologia da palavra, amador vem do latim \"amator\" que significa \"aquele que ama. E para os gregos antigos, o amor era o desejo intenso por algo considerado belo, ato este encarnado pelo deus Eros. Vale lembrar é claro quem são os pais de Eros: Poro, que representa o recurso e abundância, e Pênia, que representa a pobreza e ignorância. Isto é o ato mesmo de criação: dar algo para aquilo que é vazio. A arte opera nesse vazio. A própria filosofia opera nesse vazio, já que a dúvida é a falta de algo. Os amadores, no sentido de iniciantes em algo, normalmente tem uma visão mais apaixonada sobre o objeto de interesse. Isso configura-se como a virtude cavalheiresca, chamada Futuwah no Islã, e Futuwah significa simplesmente \"juventude\", pois somente a imaginação de um apaixonado pode conquistar uma virtude. É preciso agir como os grandes amantes: Romeu por Julieta, e Julieta por Romeu.</p>"
-  },
-  {
-    "slug": "amorandmepris",
-    "title": "Unidade das tensões",
-    "date": "Novembro 2025",
-    "excerpt": "",
-    "content": "<p>A tensão faz parte da vida humana. Devemos buscar uma unidade nas tensões internas e ao mesmo tempo dialogar com as tensões externas. Afinal é o desequilibrio natural que veio desde a expulsão do homem do Paraíso. É a questão dos temperamentos. A modernidade busca a redução das tensões, que convenhamos é impossível, buscando a universalização de aspectos mais baixos como um modelo de vida. É como se buscassem recortar pequenos aspectos da vida humana para daí irradiar alguma esperança de sentido, o que vai totalmente contra o sentido de educação na Grécia Antiga, a famosa Paideia, que significava em resumo, a formação integral do homem. Para alguns existe apenas a vida enquanto dieta, para outros apenas a vida enquanto ler livros, ou enquanto apenas trabalhar, ou enquanto apenas educar os filhos, etc. Isso é basicamente negar o dom mesmo que é o ato de ser humano. Como uma vez disse Ortega y Gasset: \"Eu sou eu e minhas circunstâncias, e se não salvo a elas, não me salvo a mim.\". Ralph Waldo Emerson o disse também com outras palavras: \"O homem é feito da mesma matéria de que são feitos os acontecimentos\".</p>"
-  },
-  {
-    "slug": "conselhodogato",
-    "title": "Agir é informação",
-    "date": "Janeiro 2026",
-    "excerpt": "",
-    "content": "<p>Alice: — Gato Cheshire... pode me dizer qual o caminho que eu devo tomar?   Gato de Cheshire: — Isso depende muito do lugar para onde você quer ir.   Alice: — Eu não sei para onde ir! – disse Alice.   Gato de Cheshire: — Se você não sabe para onde ir, qualquer caminho serve.</p>\n<p>Agir é a melhor maneira de obter informações. O Gato de Cheshire não fala sobre a decisão de qual caminho seguir, mas sim sobre a decisão de começar a andar. Simples assim.</p>"
-  },
-  {
     "slug": "filmes",
     "title": "Filmes favoritos",
     "date": "Fevereiro 2026",
@@ -38,15 +17,36 @@ export const notes: Note[] = [
   {
     "slug": "kintaro",
     "title": "Golden Boy",
-    "date": "February 2026",
+    "date": "Fevereiro 2026",
     "excerpt": "",
     "content": "<p>Rolando o feed do Youtube, deparei-me com um vídeo cuja thumb tinha inscrito \"O que aprendi com esse anime\". Cliquei e na descrição vi que era um anime chamado 'Golden Boy'. Nem pensei em assistir ao vídeo. Não sei porquê, mas fui imediatamente atrás de assistir ao anime. Eu queria ver o que EU poderia aprender com ele. Afinal, aprender é  muito bom, não é mesmo?</p>\n<p>Confesso, é um anime muito engraçado pelo exagero e sexualização banal. Ri muito. No entanto, aprendi algo. A vontade é uma força monstruosa. Nunca tinha entendido essa frase de um professor meu. Até encontrar 'Golden Boy', pois uma imagem vale mais que mil palavras. Kintaro não é gênio, mas tem uma virtude de estar apaixonado imprudentemente pela vida (e pelas mulheres) que é contagiante. E contagiou-me. Ele age como uma criança descobrindo o mundo. Talvez esse seja o ponto. Recentemente assisti a série The Young Pope e lembro bem de uma fala:</p>\n<blockquote>Crianças pintam o futuro com cores que a realidade desconhece.</blockquote>\n<p>E Kintaro faz isso muito bem. Principalmente com as pessoas com quem se relaciona. Elas ganham um pouco desse brilho nos olhos que ele tem por demais. Ele está sempre a dizer: \"Vivendo e aprendendo\". Clichê? Talvez. Mas acho que não precisa ir muito longe para ver a sabedoria milenar por trás dessa frase. E o mais interessante: a capacidade de Kintaro de estar apaixonado pela vida o leva a tomar a decisão sempre por aquilo que é certo e não por aquilo que convém a ele. Do amor nasce a moral e a capacidade de criar um senso identidade própria. Somente as pessoas que tomam os riscos da vida e seguem pelo que é certo a ser feito, sem nenhum pensamento de preservar-se, assim como Kintaro faz, somente elas podem ter o elemento mais aclamado e menos possuído pelas pessoas: personalidade. E para fazer isso, tem que estar embriagado. De quê? Baudelaire nos diz:</p>\n<blockquote>\"É a única questão. Para não sentir o horrível fardo do Tempo que vos esmaga os ombros e vos verga para a terra, embriagai-vos; embriagai-vos sem cessar! De vinho, de poesia ou de virtude, a vosso gosto. Mas embriagai-vos.\"</blockquote>"
   },
   {
+    "slug": "amador",
+    "title": "O dom do amador",
+    "date": "Janeiro 2026",
+    "excerpt": "",
+    "content": "<p>Quando Jepp Gambardella, no filme \"La Grande Bellezza\", diz: \"É triste ser boa, corre o risco de tornar-se ágil\", eu fiquei um tanto curioso. O que ele quis dizer com isso? Busquei em algumas entrevistas do diretor Paolo Sorrentino a resposta. Para ele, o cinema, mas também a própria vida, é o reino dos dilettantes (amadores). Apenas os amadores conseguem ter liberdade criativa, pois sempre encontram-se na nascente — onde as ideias vêm infinitas pelo seio da terra, e não no oceano — onde tudo se dilui. Além disso, todo artista em essência é um amador, pois na etimologia da palavra, amador vem do latim \"amator\" que significa \"aquele que ama. E para os gregos antigos, o amor era o desejo intenso por algo considerado belo, ato este encarnado pelo deus Eros. Vale lembrar é claro quem são os pais de Eros: Poro, que representa o recurso e abundância, e Pênia, que representa a pobreza e ignorância. Isto é o ato mesmo de criação: dar algo para aquilo que é vazio. A arte opera nesse vazio. A própria filosofia opera nesse vazio, já que a dúvida é a falta de algo. Os amadores, no sentido de iniciantes em algo, normalmente tem uma visão mais apaixonada sobre o objeto de interesse. Isso configura-se como a virtude cavalheiresca, chamada Futuwah no Islã, e Futuwah significa simplesmente \"juventude\", pois somente a imaginação de um apaixonado pode conquistar uma virtude. É preciso agir como os grandes amantes: Romeu por Julieta, e Julieta por Romeu.</p>"
+  },
+  {
+    "slug": "conselhodogato",
+    "title": "Agir é informação",
+    "date": "Janeiro 2026",
+    "excerpt": "",
+    "content": "<p>Alice: — Gato Cheshire... pode me dizer qual o caminho que eu devo tomar?   Gato de Cheshire: — Isso depende muito do lugar para onde você quer ir.   Alice: — Eu não sei para onde ir! – disse Alice.   Gato de Cheshire: — Se você não sabe para onde ir, qualquer caminho serve.</p>\n<p>Agir é a melhor maneira de obter informações. O Gato de Cheshire não fala sobre a decisão de qual caminho seguir, mas sim sobre a decisão de começar a andar. Simples assim.</p>"
+  },
+  {
     "slug": "vivresavie",
     "title": "Sobre falar em Vivre Sa Vie",
-    "date": "Dezembro 2026",
+    "date": "Dezembro 2025",
     "excerpt": "",
     "content": "<p>Tenho pensado em uma cena do filme \"Vivre Sa Vie\" de Jean-Luc Godard, quando ocorre o diálogo entre Nana e um filósofo. Há um momento em que ele diz: \"Eu acredito que aprendemos a falar bem quando renunciamos à vida por algum tempo. É quase... o preço. Falar é quase uma ressurreição em relação à vida. Quando falamos é uma outra vida de quando não falamos. Então, para viver falando deve-se passar pela morte da vida sem falar. Há uma certa regra ascética que te impede de falar bem até olharmos a vida com desapego.\"</p>"
+  },
+  {
+    "slug": "amorandmepris",
+    "title": "Unidade das tensões",
+    "date": "Novembro 2025",
+    "excerpt": "",
+    "content": "<p>A tensão faz parte da vida humana. Devemos buscar uma unidade nas tensões internas e ao mesmo tempo dialogar com as tensões externas. Afinal é o desequilibrio natural que veio desde a expulsão do homem do Paraíso. É a questão dos temperamentos. A modernidade busca a redução das tensões, que convenhamos é impossível, buscando a universalização de aspectos mais baixos como um modelo de vida. É como se buscassem recortar pequenos aspectos da vida humana para daí irradiar alguma esperança de sentido, o que vai totalmente contra o sentido de educação na Grécia Antiga, a famosa Paideia, que significava em resumo, a formação integral do homem. Para alguns existe apenas a vida enquanto dieta, para outros apenas a vida enquanto ler livros, ou enquanto apenas trabalhar, ou enquanto apenas educar os filhos, etc. Isso é basicamente negar o dom mesmo que é o ato de ser humano. Como uma vez disse Ortega y Gasset: \"Eu sou eu e minhas circunstâncias, e se não salvo a elas, não me salvo a mim.\". Ralph Waldo Emerson o disse também com outras palavras: \"O homem é feito da mesma matéria de que são feitos os acontecimentos\".</p>"
   }
 ]
---
+++ content/notes/python.md
@@ -0,0 +1,17 @@
+---
+title: "Por que escolhi Python?"
+date: "2026-02-4"
+---
+
+Quando li o livro "A sabedoria de Charlie Munger", encontrei nele um framework de pensamento valiosíssimo. Uso-o para questões pragmáticas, como qual linguagem de programação aprender. Esse framework chama-se Latticework of Mental Models. Não irei adentrar no conceito, pois já fiz um post explicando-o. Através dele, escolhi a linguagem de programação na qual me debruçaria.
+
+Eu não buscava uma ferramenta qualquer, mas uma que lidasse bem com a mutabilidade do mundo,e principalmente da tecnologia. Usei então alguns modelos mentais como:
+- Área de Superfície: Qual linguagem possui um maior oceano para navegar(criar)?
+- Canivete Suíço: Qual linguagem seria capaz de fazer todas as coisas razoavelmente bem?
+- Princípio de Pareto: Qual linguagem me fornece 80% do resultado buscado com 20% de esforço?
+- Efeito Lindy: Qual linguagem tem mais potencial de antifragilidade?
+
+Dentro desses parâmetros, Python foi o campeão com folga. Com a difusão de acesso da Inteligência Artificial, nem poderia ter dúvidas. Afinal eu precisava de adaptabilidade estratégica. Posso criar facilmente qualquer coisa com Python: uma automação, um site, um jogo, um processo de web scraping, etc. 
+
+
+
